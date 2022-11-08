98694ada6c895db2c4df8a2074f15d99959bb216
Mohamad J. Makki <malanbaki@transforat.com>
2022-11-08 10:06:40 +0300

    Angular Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './compo/user/user.component';
+import {RouterOutlet} from "@angular/router";
+import { UserListComponent } from './compo/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterOutlet
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
