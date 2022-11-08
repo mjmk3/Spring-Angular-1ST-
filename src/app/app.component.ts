f74be084706a2f31239e9112abbdf8b9fb8d5826
Mohamad J. Makki <malanbaki@transforat.com>
2022-11-08 09:59:32 +0300

    Angular Client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularClient';
+  title: string;
+  constructor() {
+    this.title = 'Spring Boot - Angular Application';
+  }
 }
